96f6fc7eb074f681788a2202c6cb0d3846253aee
Tyler Chang <tylerchang888@gmail.com>
2022-09-27 17:24:11 -0600

    Added comments

--- src/blink.py
+++ src/blink.py
@@ -2,10 +2,14 @@ import pigpio
 
 def nic_send(bits):
     pi1 = pigpio.pi()
+
+    # Extracting the bit values and storing them
     port1_xmit_status = bits[0]
     port2_xmit_status = bits[1]
     port3_xmit_status = bits[2]
     port4_xmit_status = bits[3]
+
+    # Writing the bit values to the corresponding ports
 
     # Port 1 Output/Xmit
     pi1.write(27,int(port1_xmit_status))
@@ -20,28 +24,33 @@ def nic_recv():
     pi2 = pigpio.pi()
     # Port 1 Input/Recv
     port1_status = str(pi2.read(26))
+    # Flipping the bit values since everything is reversed
     if port1_status =="1":
         port1_status = "0"
     elif port1_status == "0":
         port1_status = "1"
     # Port 2 Input/Recv
     port2_status = str(pi2.read(24))
+    # Flipping the bit values since everything is reversed
     if port2_status == "1":
         port2_status = "0"
     elif port2_status == "0":
         port2_status = "1"
     # Port 3 Input/Recv
     port3_status = str(pi2.read(22))
+    # Flipping the bit values since everything is reversed
     if port3_status == "1":
         port3_status = "0"
     elif port3_status == "0":
         port3_status = "1"
     # Port 4 Input/Recv
     port4_status = str(pi2.read(20))
+    # Flipping the bit values since everything is reversed
     if port4_status == "1":
         port4_status = "0"
     elif port4_status == "0":
         port4_status = "1"
+    # Putting together the bit string
     binary_string = port1_status + port2_status + port3_status + port4_status
     return binary_string
 
