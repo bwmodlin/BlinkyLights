363d1176196d893f80a5178beb5655789bfa10a5
bwmodlin <b_modlin@coloradocollege.edu>
2022-09-27 21:01:23 -0600

    Refactor extraneous if statements

--- src/blink.py
+++ src/blink.py
@@ -22,35 +22,20 @@ def nic_send(bits):
 
 def nic_recv():
     pi2 = pigpio.pi()
+
+    # Must flip the 1's and 0's because LEDs are incorrectly inverted
     # Port 1 Input/Recv
-    port1_status = str(pi2.read(26))
-    # Flipping the bit values since everything is reversed
-    if port1_status =="1":
-        port1_status = "0"
-    elif port1_status == "0":
-        port1_status = "1"
+    port1_status = str(1 - pi2.read(26))
+
     # Port 2 Input/Recv
-    port2_status = str(pi2.read(24))
-    # Flipping the bit values since everything is reversed
-    if port2_status == "1":
-        port2_status = "0"
-    elif port2_status == "0":
-        port2_status = "1"
+    port2_status = str(1 - pi2.read(24))
+
     # Port 3 Input/Recv
-    port3_status = str(pi2.read(22))
-    # Flipping the bit values since everything is reversed
-    if port3_status == "1":
-        port3_status = "0"
-    elif port3_status == "0":
-        port3_status = "1"
+    port3_status = str(1 - pi2.read(22))
+
     # Port 4 Input/Recv
-    port4_status = str(pi2.read(20))
-    # Flipping the bit values since everything is reversed
-    if port4_status == "1":
-        port4_status = "0"
-    elif port4_status == "0":
-        port4_status = "1"
-    # Putting together the bit string
+    port4_status = str(1 - pi2.read(20))
+
     binary_string = port1_status + port2_status + port3_status + port4_status
     return binary_string
 
